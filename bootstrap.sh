0673659d5f6dcbc2b77474f0f9ae7e74e63bbf6f
Aaron Shafovaloff <ashafovaloff@instructure.com>
2013-09-20 14:11:43 -0600

    don't use quiet + misc fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,25 +4,32 @@ echo
 echo Setting up VM for Canvas LMS. This should take about 20 minutes. Get some breakfast.
 echo
 
-sudo add-apt-repository ppa:chris-lea/redis-server -y
-sudo add-apt-repository ppa:chris-lea/node.js -y
-sudo apt-get update
+sudo su
+apt-get update
 
-sudo apt-get install git-core python-software-properties python g++ make \
+apt-get install git-core python-software-properties python g++ make \
   ruby1.9.1 ruby1.9.1-dev zlib1g-dev rubygems1.9.1 \
   libxml2-dev libxslt1-dev libsqlite3-dev libhttpclient-ruby \
   imagemagick irb1.9.1 libxmlsec1-dev postgresql \
   python-software-properties libcurl3-dev libpq-dev \
-  redis-server expect-lite nodejs -y --quiet
+  expect-lite -y
 
-sudo su -l postgres -c 'psql -c "CREATE ROLE root WITH SUPERUSER LOGIN;"'
-sudo su -l postgres -c 'psql -c "CREATE ROLE vagrant WITH SUPERUSER LOGIN;"'
-sudo su -l postgres -c 'createdb canvas_development'
-sudo su -l postgres -c 'createdb canvas_queue_development'
+add-apt-repository ppa:chris-lea/redis-server -y
+apt-get update
+apt-get install redis-server -y
 
-sudo npm install -g coffee-script -y
+add-apt-repository ppa:chris-lea/node.js -y
+apt-get update
+apt-get install nodejs -y
 
-sudo update-alternatives --set ruby /usr/bin/ruby1.9.1
+su -l postgres -c 'psql -c "CREATE ROLE root WITH SUPERUSER LOGIN;"'
+su -l postgres -c 'psql -c "CREATE ROLE vagrant WITH SUPERUSER LOGIN;"'
+su -l postgres -c 'createdb canvas_development'
+su -l postgres -c 'createdb canvas_queue_development'
+
+npm install -g coffee-script -y
+
+update-alternatives --set ruby /usr/bin/ruby1.9.1
 
 cp /vagrant/canvas/config/database.yml.example /vagrant/canvas/config/database.yml
 cp /vagrant/config/security.yml /vagrant/canvas/config/security.yml
@@ -32,8 +39,8 @@ cp /vagrant/config/redis.yml /vagrant/canvas/config/redis.yml
 cp /vagrant/config/domain.yml /vagrant/canvas/config/domain.yml
 
 cd /vagrant/canvas
-sudo gem install bundler
-sudo bundle install --quiet --without mysql
+gem install bundler
+bundle install --without mysql
 expect-lite -c /vagrant/config/db-initial-setup.elt
-sudo bundle exec rake canvas:compile_assets
-sudo bundle exec script/server
+bundle exec rake canvas:compile_assets
+bundle exec script/server
